06c8865be158e668230e52e9ce8e69ba150a52b8
jlrm01-uni <jorge.rodriguez@guayama.inter.edu>
2021-04-12 08:58:16 -0400

    Usé lamdas para los listeners de OnClick

--- app/src/main/java/com/example/interfacestest/MainActivity.kt
+++ app/src/main/java/com/example/interfacestest/MainActivity.kt
@@ -8,48 +8,37 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     private var value = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Estos son mis botones
-        findViewById<Button>(R.id.buttonSumar).setOnClickListener(this)
-        findViewById<Button>(R.id.buttonRestar).setOnClickListener(this)
-        findViewById<Button>(R.id.buttonReset).setOnClickListener(this)
-        findViewById<Button>(R.id.buttonSmallerFont).setOnClickListener(this)
-        findViewById<Button>(R.id.buttonBiggerFont).setOnClickListener(this)
-        findViewById<Button>(R.id.buttonHideShow).setOnClickListener(this)
-
-    }
-
-    override fun onClick(v: View?) {
         val text_view = findViewById<TextView>(R.id.textView)
 
-        when (v?.id) {
-            R.id.buttonSumar -> {
-                value+=1
-                text_view.text = value.toString()
-            }
-            R.id.buttonRestar -> {
-                value-=1
-                text_view.text = value.toString()
-            }
-            R.id.buttonReset -> {
-                value=0
-                text_view.text = value.toString()
-            }
-            R.id.buttonSmallerFont -> {
-                text_view.textSize = 20f
-            }
-            R.id.buttonBiggerFont -> {
-                text_view.textSize = 250f
-            }
-            R.id.buttonHideShow -> {
-                text_view.isVisible = !text_view.isVisible
-            }
+        // Estos son mis botones
+        findViewById<Button>(R.id.buttonSumar).setOnClickListener {
+            value+=1
+            text_view.text = value.toString()
         }
+        findViewById<Button>(R.id.buttonRestar).setOnClickListener {
+            value-=1
+            text_view.text = value.toString()
+        }
+        findViewById<Button>(R.id.buttonReset).setOnClickListener {
+            value=0
+            text_view.text = value.toString()
+        }
+        findViewById<Button>(R.id.buttonSmallerFont).setOnClickListener {
+            text_view.textSize = 20f
+        }
+        findViewById<Button>(R.id.buttonBiggerFont).setOnClickListener {
+            text_view.textSize = 250f
+        }
+        findViewById<Button>(R.id.buttonHideShow).setOnClickListener {
+            text_view.isVisible = !text_view.isVisible
+        }
+
     }
 }
