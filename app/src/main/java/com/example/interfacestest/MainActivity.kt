489c016cd6dcb72624b52cb1cc2a7a7365c6699e
jlrm01-uni <jorge.rodriguez@guayama.inter.edu>
2021-04-12 09:02:21 -0400

    Arreglé el nombre de una variable.

--- app/src/main/java/com/example/interfacestest/MainActivity.kt
+++ app/src/main/java/com/example/interfacestest/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.interfacestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
@@ -15,34 +13,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text_view = findViewById<TextView>(R.id.textView)
+        val textView = findViewById<TextView>(R.id.textView)
 
         // Estos son mis botones
         findViewById<Button>(R.id.buttonSumar).setOnClickListener {
             value+=1
-            text_view.text = value.toString()
+            textView.text = value.toString()
         }
 
         findViewById<Button>(R.id.buttonRestar).setOnClickListener {
             value-=1
-            text_view.text = value.toString()
+            textView.text = value.toString()
         }
 
         findViewById<Button>(R.id.buttonReset).setOnClickListener {
             value=0
-            text_view.text = value.toString()
+            textView.text = value.toString()
         }
 
         findViewById<Button>(R.id.buttonSmallerFont).setOnClickListener {
-            text_view.textSize = 20f
+            textView.textSize = 20f
         }
 
         findViewById<Button>(R.id.buttonBiggerFont).setOnClickListener {
-            text_view.textSize = 250f
+            textView.textSize = 250f
         }
 
         findViewById<Button>(R.id.buttonHideShow).setOnClickListener {
-            text_view.isVisible = !text_view.isVisible
+            textView.isVisible = !textView.isVisible
         }
     }
 }
